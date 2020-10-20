87cc5cd1261a31251dad5fbda8c292fdaffc41a4
VKotrunov <57618856+VKotrunov@users.noreply.github.com>
2020-10-20 11:04:17 +0200

    Agregar archivos de proyecto.

---
+++ UT2_Ac3_CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UT2_Ac3_CaracteresLimitados
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void inputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int longitud = inputTextBox.Text.Length;
+            if (longitud == 140)
+                inputTextBox.IsReadOnly = true;
+            caracteresTextBlock.Text = longitud + "/140";
+        }
+    }
+}
